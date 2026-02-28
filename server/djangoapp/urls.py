580e709b0ea6c0ebb3f358dd55ddc4a3da886636
xmkabel <mkabel190@gmail.com>
2026-02-28 03:11:43 -0500

    update

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 
     # path for add a review view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -97,9 +97,17 @@ def get_dealerships(request, state="All"):
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
-    endpoint = "/fetchReviews/dealer/" + str(dealer_id)
-    reviews = get_request(endpoint)
-    return JsonResponse({"status": 200, "reviews": reviews})
+    # if dealer id has been provided
+    if(dealer_id):
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status":200,"reviews":reviews})
+    else:
+        return JsonResponse({"status":400,"message":"Bad Request"})
 
 
 # Create a `get_dealer_details` view to render the dealer details
@@ -112,3 +120,13 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+def add_review(request):
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
